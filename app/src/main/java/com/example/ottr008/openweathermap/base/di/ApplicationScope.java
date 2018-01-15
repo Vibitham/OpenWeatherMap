455248b26717ddad0bf23cf3dee81dc3df576286
Nikhil Vijayakumar <nvijayakumar@opentrends.net>
2018-01-15 14:57:26 +0530

    update dagger version

---
+++ app/src/main/java/com/example/ottr008/openweathermap/base/di/ApplicationScope.java
@@ -0,0 +1,12 @@
+package com.example.ottr008.openweathermap.base.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApplicationScope {
+}
